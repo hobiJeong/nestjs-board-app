f5197b7daaf8086009b05e7f35f2dcafc49288ec
hobiJeong <jjb26433@naver.com>
2023-08-10 18:02:06 +0900

    biho : Get API 틀 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
 
-    constructor(private boardsSerivce: BoardsService) {}
+    constructor(private boardsSerivce: BoardsService) { }
 
+    @Get('/')
+    getAllBoard() {
+        return this.boardsSerivce.getAllBoards();
+    }
 
-    this.boardsSerivce.getAllBoards();
+    
 }
