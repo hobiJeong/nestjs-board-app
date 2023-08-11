b853d5068d8b63d26a968f0958fef182143afa2e
hobiJeong <jjb26433@naver.com>
2023-08-11 10:23:03 +0900

    biho : DTO 생성, 적용

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
-import { v1 as uuid } from 'uuid';
+import { v1 as uuid } from "uuid";
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -11,7 +12,9 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+
         const board: Board = {
             id: uuid(),
             title,
@@ -22,4 +25,5 @@ export class BoardsService {
         this.boards.push(board);
         return board;
     }
+
 }
