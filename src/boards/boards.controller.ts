f523f47fae71856c92f4d3c00409bd8f8933fc74
hobiJeong <jjb26433@naver.com>
2023-08-23 13:40:42 +0900

    biho : 필요없는 interface board 삭제, Inject repository

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,42 +1,41 @@
 import { Body, Param, Controller, Get, Post, Delete, Patch, ValidationPipe, UsePipes } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-
     constructor(private boardsSerivce: BoardsService) { }
 
-    @Get("/")
-    getAllBoard(): Board[] {
-        return this.boardsSerivce.getAllBoards();
-    }
+    // @Get("/")
+    // getAllBoard(): Board[] {
+    //     // return this.boardsSerivce.getAllBoards();
+    // }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
-        return this.boardsSerivce.createBoard(createBoardDto);
-    }
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board {
+    //     // return this.boardsSerivce.createBoard(createBoardDto);
+    // }
 
-    @Get("/:id")
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardsSerivce.getBoardById(id)
-    }
+    // @Get("/:id")
+    // getBoardById(@Param('id') id: string): Board {
+    //     // return this.boardsSerivce.getBoardById(id)
+    // }
 
-    @Delete("/:id")
-    deleteBoard(@Param("id") id:string): void {
-        this.boardsSerivce.deleteBoard(id);
-    }
+    // @Delete("/:id")
+    // deleteBoard(@Param("id") id:string): void {
+    //     // this.boardsSerivce.deleteBoard(id);
+    // }
     
-    @Patch("/:id/status")
-    updateBoardStatus(
-        @Param("id") id: string,
-        @Body("status", BoardStatusValidationPipe) status: BoardStatus,
-    ) {
-        return this.boardsSerivce.updateBoardStatus(id, status);
-    }
+    // @Patch("/:id/status")
+    // updateBoardStatus(
+    //     @Param("id") id: string,
+    //     @Body("status", BoardStatusValidationPipe) status: BoardStatus,
+    // ) {
+    //     // return this.boardsSerivce.updateBoardStatus(id, status);
+    // }
 }
